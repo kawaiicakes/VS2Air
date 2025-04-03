75afb575f64bb62ef42b85606b41955e92213ed7
kawaiicakes <vomitmopper@gmail.com>
2025-04-02 18:34:24 -0600

    Prepared VS phys properties and made ImmortalAirBlock not replaceable.

--- common/src/main/java/io/github/kawaiicakes/vs2air/block/ImmortalAirBlock.java
+++ common/src/main/java/io/github/kawaiicakes/vs2air/block/ImmortalAirBlock.java
@@ -3,11 +3,24 @@ package io.github.kawaiicakes.vs2air.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
 
+@SuppressWarnings("deprecation")
 public class ImmortalAirBlock extends VS2AirBlock {
     public ImmortalAirBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public boolean canBeReplaced(BlockState blockState, Fluid fluid) {
+        return false;
+    }
+
+    @Override
+    public boolean canBeReplaced(BlockState blockState, BlockPlaceContext blockPlaceContext) {
+        return false;
     }
 
     @Override
