d51b265b5bd22ce6e1dcb4ce1f47cb4343b700a8
kawaiicakes <vomitmopper@gmail.com>
2025-04-08 15:43:28 -0600

    Unnecessary mixin now that material has been changed

--- fabric/src/main/java/io/github/kawaiicakes/vs2air/fabric/VS2AirFabric.java
+++ fabric/src/main/java/io/github/kawaiicakes/vs2air/fabric/VS2AirFabric.java
@@ -1,12 +1,24 @@
 package io.github.kawaiicakes.vs2air.fabric;
 
+import io.github.kawaiicakes.vs2air.block.StandardAirBlock;
+import io.github.kawaiicakes.vs2air.block.VS2AirBlock;
 import net.fabricmc.api.ModInitializer;
 
 import io.github.kawaiicakes.vs2air.VS2Air;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public final class VS2AirFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         VS2Air.init();
+
+        Block block = new StandardAirBlock(VS2AirBlock.defaultProperties());
+        Registry.register(Registry.BLOCK, new ResourceLocation("vs2air", "standard"), block);
+        Registry.register(Registry.ITEM, new ResourceLocation("vs2air", "standard"), new BlockItem(block, new Item.Properties()));
+
     }
 }
