e48aa2b55cb1c789f7d5f8b4330ff544e7ce4114
kawaiicakes <vomitmopper@gmail.com>
2025-04-07 18:52:38 -0600

    poggers vs2 seeding

--- common/src/main/java/io/github/kawaiicakes/vs2air/VS2Air.java
+++ common/src/main/java/io/github/kawaiicakes/vs2air/VS2Air.java
@@ -1,6 +1,7 @@
 package io.github.kawaiicakes.vs2air;
 
 public final class VS2Air {
+    // TODO: notify quoissants / 233675450570768384 on discord when this releases
     public static final String MOD_ID = "vs2air";
 
     public static void init() {}
